295921f0a38beead8fe213c49ca93a2160290e43
Amazingfeel <357822810@qq.com>
2016-08-22 09:42:57 +0800

    腾讯QQ登录  只是添加 SDK 没有代码实现 应用审核信息没有填写完整

---
+++ cocopod3/cocopod3/MeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MeViewController.h
+//  cocopod3
+//
+//  Created by ZYS on 16/8/22.
+//  Copyright © 2016年 jingqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeViewController : UIViewController
+
+@end
